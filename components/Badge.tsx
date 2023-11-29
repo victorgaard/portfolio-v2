8b8529ec0535fc9fbb9bcdee12dd26e0eb4069ce
Victor F. Santos <victor.gaard@gmail.com>
2023-11-29 18:24:17 +0100

    feat: favico

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -9,7 +9,7 @@ function Badge({
   return (
     <div
       className={cn(
-        "rounded-full bg-zinc-200 px-4 py-1.5 text-xs text-zinc-900 transition-all dark:bg-zinc-900 dark:text-white",
+        "rounded-full bg-zinc-200/50 px-4 py-1.5 text-xs text-zinc-900 transition-all dark:bg-zinc-900 dark:text-white",
         className,
       )}
       {...rest}
