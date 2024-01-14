daca3b203f7122d56f1bc6dcae52902e903027f2
Victor F. Santos <victor.gaard@gmail.com>
2024-01-14 15:58:04 +0100

    fix: timeAgo fn

--- utils/date.ts
+++ utils/date.ts
@@ -8,19 +8,21 @@ export function format(date: Date) {
 export function timeAgo(start: Date, end: Date | null) {
   const endDate = end || new Date();
   const yearDiff = endDate.getFullYear() - start.getFullYear();
-  const monthDiff = endDate.getMonth() - (start.getMonth());
-
+  const monthDiff = endDate.getMonth() - start.getMonth() + 1;
+  const totalMonths = yearDiff * 12 + monthDiff;
+  const years = Math.floor(totalMonths / 12);
+  const months = totalMonths % 12;
   let result = "";
 
-  if (yearDiff > 0) {
-    result += `${yearDiff} ${yearDiff === 1 ? "year" : "years"}`;
+  if (years > 0) {
+    result += `${years} ${years === 1 ? "year" : "years"}`;
   }
 
-  if (monthDiff > 0) {
+  if (months > 0) {
     if (result.length > 0) {
       result += " ";
     }
-    result += `${monthDiff} ${monthDiff === 1 ? "month" : "months"}`;
+    result += `${months} ${months === 1 ? "month" : "months"}`;
   }
 
   if (result.length === 0) {
